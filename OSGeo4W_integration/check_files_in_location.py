47b916de56891bc24963c2de26757b2c8609719a
SiegmannGiS <marcel.siegmann@uibk.ac.at>
2016-05-22 13:58:50 +0200

    Check files in every grass location and print them

--- OSGeo4W_integration/check_files_in_location.py
+++ OSGeo4W_integration/check_files_in_location.py
@@ -1,24 +1,26 @@
 import grass.script as gscript
 import grass.script.setup as gsetup
+import os
 
 ###########
 # launch session
 gisbase = r"C:\OSGEO4~1\apps\grass\grass-7.0.4"
 gisdb = r"C:\grassdb"
-location = "vernagtferner"
 mapset = "PERMANENT"
 
-gsetup.init(gisbase,gisdb, location, mapset)
+for i,location in enumerate(sorted(os.listdir(gisdb))):
+    print("\n\n"+location)
+    gsetup.init(gisbase,gisdb, location, mapset)
 
-gscript.message('Current GRASS GIS 7 environment:')
-print(gscript.gisenv())
+    gscript.message('Current GRASS GIS 7 environment:')
+    print(gscript.gisenv())
 
-gscript.message('Available raster maps:')
-for rast in gscript.list_strings(type='rast'):
-    print(rast)
+    gscript.message('Available raster maps:')
+    for rast in gscript.list_strings(type='rast'):
+        print(rast)
 
-gscript.message('Available vector maps:')
-for vect in gscript.list_strings(type='vect'):
-    print(vect)
+    gscript.message('Available vector maps:')
+    for vect in gscript.list_strings(type='vect'):
+        print(vect)
 
-print("done")
+    print("done")
