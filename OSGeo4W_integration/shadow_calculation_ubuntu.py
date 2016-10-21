a71a676b7005764f33142caed65b5b8897538a49
SiegmannGiS <marcel.siegmann@uibk.ac.at>
2016-10-21 10:33:42 +0200

    powerhorse calculation

---
+++ OSGeo4W_integration/shadow_calculation_ubuntu.py
@@ -0,0 +1,51 @@
+import grass.script as gscript
+import grass.script.setup as gsetup
+import os
+from grass import script
+from datetime import datetime
+
+# Init grass
+gisbase = r"grass"
+gisdb = r"/home/marcel/grassdb"
+mapset = "PERMANENT"
+location = "vernagtferner"
+gsetup.init(gisbase, gisdb, location, mapset)
+
+# set region
+script.run_command("g.region", rast="dem")
+print("region set")
+#
+#Paths
+with open("%s.txt" %location) as fobj:
+    lines = fobj.readlines()
+
+path1 = "/home/marcel/Master/shadows/%s" %location
+
+# iterate over date of images
+for i, name in enumerate(lines):
+    name = name.strip()
+    if i > 0:
+    # if name == "2014-10-25_10-00.jpg":
+        print("\nProgress: %.2f" % (((i+1) / float(len(lines))) * 100))
+
+        date = datetime.strptime(name, "%Y-%m-%d_%H-%M")
+        civil_time = "0"
+        yday = date.timetuple().tm_yday
+        Minute = "%i" % (int(date.minute) / 60. * 100.)
+        Moment = "%s.%s" % (date.hour, Minute)
+        os.system("r.sun elevin=dem aspin=dem_aspect slopein=dem_slope latin=dem_lat longin=dem_lon incidout=shadow"
+              " civiltime=%s day=%s time=%s --overwrite" % (civil_time, yday, Moment))
+        os.system("r.out.ascii input=shadow output=%s.asc null=-9999.0 -h" % (
+        os.path.join(path1, name)))
+
+
+# name = "2016-07-10_05-32.jpg"
+# date = datetime(2016,7,10,5,32)
+# civil_time = "0"
+# yday = date.timetuple().tm_yday
+# Minute = "%i" % (int(date.minute) / 60. * 100.)
+# Moment = "%s.%s" % (date.hour, Minute)
+# os.system("r.sun elevation=dem aspect=dem_aspect slope=dem_slope lat=dem_lat long=dem_lon incidout=shadow"
+#           " civil_time=%s day=%s time=%s --overwrite" % (civil_time, yday, Moment))
+# os.system("r.out.ascii input=shadow output=%s.asc null_value=-9999.0 -h --overwrite" % (
+#     os.path.join(path1, name.split(".")[0])))
